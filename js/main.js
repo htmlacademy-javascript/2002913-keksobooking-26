be9358efe7f30f7da3ef0d92937dbdd5630244ab
SemenK <samkrekotyn@gmail.com>
2022-06-09 15:11:37 +0300

    fix arrow function

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 // eslint-disable-next-line no-unused-vars
 const getRandomInteger = (min, max) => {
   if (min > max || min < 0 || max <= 0) {
-    return ('Задан неверный диапазон! Укажите другие числа.');
+    throw new RangeError ('Задан неверный диапазон! Укажите другие числа.');
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
@@ -10,7 +10,7 @@ const getRandomInteger = (min, max) => {
 //Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно. Источник MDN
 const getRandomNumber = (min, max, decimalPoint = 5) => {
   if (min > max || min < 0 || max <= 0) {
-    return ('Задан неверный диапазон! Укажите другие числа.');
+    throw new RangeError (`Задан неверный диапазон! Укажите другие числа. Параметр должен быть между ${min} и ${max}`);
   }
   const random = Math.random() * (max - min) + min;
   return +(random.toFixed(decimalPoint));
