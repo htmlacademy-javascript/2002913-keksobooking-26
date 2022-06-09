1ca35d33fadec5a8a9754cd61493c30de4e72067
Keks <keks@htmlacademy.ru>
2022-06-09 15:12:49 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,16 +1,20 @@
 //Функция, возвращающая случайное целое число из переданного диапазона включительно,источник https://schoolsw3.com/js/js_random.php
-function getRandomFloor(min, max) {
+// eslint-disable-next-line no-unused-vars
+const getRandomInteger = (min, max) => {
+  if (min > max || min < 0 || max <= 0) {
+    throw new RangeError ('Задан неверный диапазон! Укажите другие числа.');
+  }
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
 //Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно. Источник MDN
-function getRandomNumber(min, max, decimalPoint) {
+const getRandomNumber = (min, max, decimalPoint = 5) => {
   if (min > max || min < 0 || max <= 0) {
-    return ('Задан неверный диапазон! Укажите другие числа.');
+    throw new RangeError (`Задан неверный диапазон! Укажите другие числа. Параметр должен быть между ${min} и ${max}`);
   }
   const random = Math.random() * (max - min) + min;
   return +(random.toFixed(decimalPoint));
-}
+};
 
-getRandomFloor(1,440);
-getRandomNumber(2,34,4);
+getRandomInteger(1, 440);
+getRandomNumber(2, 34, 4);
