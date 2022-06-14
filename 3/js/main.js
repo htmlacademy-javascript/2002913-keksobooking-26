a921e0c2e9e9d26599c020b058975fb6a61ab18d
Keks <keks@htmlacademy.ru>
2022-06-15 00:55:21 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,25 +1,10 @@
-//Функция, возвращающая случайное целое число из переданного диапазона включительно,источник https://schoolsw3.com/js/js_random.php
-// eslint-disable-next-line no-unused-vars
-const getRandomPositiveInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-//Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно. Источник MDN
-function getRandomNumber(min, max, decimalPoint = 5) {
-  if (min > max || min < 0 || max <= 0) {
-    throw new RangeError(`Задан неверный диапазон! Укажите другие числа. Параметр должен быть между ${min} и ${max}`);
-  }
-  const random = Math.random() * (max - min) + min;
-  return +(random.toFixed(decimalPoint));
-}
-
-
+//imports
+//const
 const AVATARS = [];
 for (let i = 1; i <=10; i++) {
-  AVATARS.push(i);
+  if (i ===  10) {
+    AVATARS.push(i);}
+  AVATARS.push(`${0}${i}`);
 }
 
 const TITLES = [
@@ -70,35 +55,71 @@ const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
 
+
+const APARTMENTS_AMOUNT = 10;
+//selectors
+//functions,list
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getArray = (arr) => {
+  const maxLength = arr.length;
+  const lengthOfArray = getRandomPositiveInteger(1, maxLength);
+  const array = [];
+
+  while (array.length < lengthOfArray) {
+    const indexOfEl = getRandomPositiveInteger(0, maxLength - 1);
+    const el = arr[indexOfEl];
+
+    if (!array.includes(el)) {
+      array.push(el);
+    }
+  }
+  return array;
+};
+
+const getRandomNumber = (min, max, decimalPoint = 5) => {
+  if (min > max || min < 0 || max <= 0) {
+    throw new RangeError(`Задан неверный диапазон! Укажите другие числа. Параметр должен быть между ${min} и ${max}`);
+  }
+  const random = Math.random() * (max - min) + min;
+  return +(random.toFixed(decimalPoint));
+};
+
+// const randomLat =  getRandomNumber(35.65000, 35.70000);
+
+// const randomLng = getRandomNumber(139.70000, 139.80000);
+
 const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
-const createAdvertisement = () => ({
+const createAd = () => ({
   author : {
     avatar : `img/avatars/user${getRandomArrayElement(AVATARS)}.png`
   },
-
   offer : {
     title : getRandomArrayElement(TITLES),
-    address : '{{location.lat}}, {{location.lng}}',
+    address : `${location.lat}, ${location.lng}`,
     type : getRandomArrayElement(TYPES),
     rooms :  getRandomPositiveInteger(1, 6),
     price : getRandomPositiveInteger(1, 10),
     guests : getRandomPositiveInteger(1, 10),
     checkin : getRandomArrayElement(CHECKIN),
     checkout : getRandomArrayElement(CHECKOUT),
-    features : getRandomArrayElement(FEATURES),
+    features : getArray(FEATURES),
     description : getRandomArrayElement(DESCRIPTIONS),
-    photos : getRandomArrayElement(PHOTOS),
+    photos : getArray(PHOTOS),
   },
-
   location : {
     lat : getRandomNumber(35.65000, 35.70000),
     lng : getRandomNumber(139.70000, 139.80000),
   },
 });
 
-const SIMILAR_APARTMENTS_NEARBY = 10;
+const createAds = Array.from({length: APARTMENTS_AMOUNT}, createAd);
+//exe
 
-const simularAdvertisements = Array.from({length: SIMILAR_APARTMENTS_NEARBY}, createAdvertisement);
-
-simularAdvertisements();
+createAds();
