31d08ba8bd506a8078a39e1b868c4c66d1d4a2ee
Семён Крекотун <samkrekotyn@gmail.com>
2022-07-09 23:33:52 +0300

    feat(map) добавляет карту, главный маркер, маркеры поблизости, главный маркер получает событие для вывода координат в поле с адресом

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 import {setupValidation} from './user-form.js';
-import {createAds} from './data.js';
+// import {createAds} from './data.js';
 // import {renderCard} from './popup.js';
 import './map.js';
 
 
 // const mapCanvas = document.querySelector('#map-canvas');
 
-const simulateAds = createAds();
+// const simulateAds = createAds();
 // const card = renderCard(simulateAds[0]);
 
 // mapCanvas.appendChild(card);
 setupValidation();
-
 
--- js/map.js
+++ js/map.js
@@ -1,6 +1,9 @@
 import {activateForm} from './form-state.js';
+import {createAds} from './data.js';
+// import {renderCard} from './popup.js';
 
-const address = document.querySelector('.address');
+const simulateAds = createAds();
+const address = document.querySelector('#address');
 
 const map = L.map('map-canvas')
   .on('load', () => {
@@ -18,6 +21,18 @@ L.tileLayer(
   },
 ).addTo(map);
 
+const mainPinIcon = L.icon({
+  iconUrl: './img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const pinIcon = L.icon({
+  iconUrl: './img/pin.svg',
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+});
+
 const marker = L.marker(
   {
     lat: 35.65283,
@@ -25,12 +40,26 @@ const marker = L.marker(
   },
   {
     draggable: true,
+    icon: mainPinIcon,
   },
 );
+
+createAds();
 
 marker.addTo(map);
 
 marker.on('moveend', (evt) => {
-  console.log(evt.target.getLatLng());
+  const {lat, lng} = evt.target.getLatLng();
+  address.value = `${lat.toFixed(5)}, ${lng.toFixed(5)}`;
 });
 
+const creatMarker = (point) => {
+  const reularMarker = L.marker(point, {icon:pinIcon});
+  reularMarker.addTo(map);
+};
+
+simulateAds.forEach((adv) => {
+  creatMarker(adv.location, {icon: pinIcon,});
+});
+
+// console.log(pinIcon);
